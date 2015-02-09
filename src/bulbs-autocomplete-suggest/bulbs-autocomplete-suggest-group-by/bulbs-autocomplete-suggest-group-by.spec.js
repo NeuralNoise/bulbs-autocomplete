655f04c2bc3f41340e2a9e9a855717b479df9663
Andrew Kos <akos@theonion.com>
2015-02-09 16:58:59 -0600

    First test for suggest group by

--- src/bulbs-autocomplete-suggest/bulbs-autocomplete-suggest-group-by/bulbs-autocomplete-suggest-group-by.spec.js
+++ src/bulbs-autocomplete-suggest/bulbs-autocomplete-suggest-group-by/bulbs-autocomplete-suggest-group-by.spec.js
@@ -3,34 +3,55 @@
 describe('Directive: bulbsAutocompleteSuggestGroupBy', function () {
   var
     $scope,
-    $directiveScope;
+    $directiveScope,
+    BulbsAutocompleteFormatterService;
 
   beforeEach(function () {
     module('BulbsAutocomplete.suggest.formatter.service');
     module('BulbsAutocomplete.suggest.groupBy');
     module('jsTemplates');
 
-    inject(function (_$rootScope_, $compile) {
+    inject(function (_$rootScope_, $compile, _BulbsAutocompleteFormatterService_) {
        $scope = _$rootScope_.$new();
+       BulbsAutocompleteFormatterService = _BulbsAutocompleteFormatterService_;
 
        $scope.formatter = function () {};
-       $scope.grouper = function () {};
+       $scope.grouper = function () {
+         return {
+           'group 1': [{
+             name: 'item 1',
+             value: 10
+           }, {
+             name: 'item 2',
+             value: 20
+           }],
+           'group 2': [{
+             name: 'item 3',
+             value: 30
+           }, {
+             name: 'item 4',
+             value: 40
+           }]
+         };
+       };
        $scope.items = [];
        $scope.onSelect = function () {};
 
-       spyOn($scope, 'formatter');
-       spyOn($scope, 'grouper');
-       spyOn($scope, 'onSelect');
+       spyOn(BulbsAutocompleteFormatterService, 'buildFormatter');
+       spyOn($scope, 'formatter').and.callThrough();
+       spyOn($scope, 'grouper').and.callThrough();
+       spyOn($scope, 'onSelect').and.callThrough();
 
-      var element = $compile('<bulbs-autocomplete-suggest-group-by formatter="formatter" ></bulbs-autocomplete-suggest-group-by>')($scope.$new());
+      var element = $compile('<bulbs-autocomplete-suggest-group-by formatter="formatter" grouper="grouper" items="items" on-select="onSelect"></bulbs-autocomplete-suggest-group-by>')($scope.$new());
       _$rootScope_.$digest();
       $directiveScope = element.isolateScope();
     });
   });
 
-  it('should ', function () {
-
-  // TODO : fill this in
-    throw 'Not implemented yet.';
+  it('should add grouped items to the scope when items changes', function () {
+    expect(BulbsAutocompleteFormatterService.buildFormatter).toHaveBeenCalled();
+    expect($scope.grouper).toHaveBeenCalled();
+    expect($directiveScope.formattedGroupedItems).toBeDefined();
   });
+
 });
