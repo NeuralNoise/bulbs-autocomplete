4611b31def4558eebc74fc5e053cc7d6dd15c031
Andrew Kos <akos123@gmail.com>
2017-01-04 15:39:00 -0600

    updated dist

--- dist/bulbs-autocomplete.js
+++ dist/bulbs-autocomplete.js
@@ -181,12 +181,12 @@ angular.module('BulbsAutocomplete', [
 // Source: .tmp/bulbs-autocomplete-templates.js
 angular.module('BulbsAutocomplete').run(['$templateCache', function($templateCache) {
 $templateCache.put('src/bulbs-autocomplete-suggest/bulbs-autocomplete-suggest-group-by/bulbs-autocomplete-suggest-group-by.html',
-    "<ul><li class=bulbs-autocomplete-group-container ng-repeat=\"group in groupedItems\"><div class=bulbs-autocomplete-group-key>{{ group[0] }}</div><ul class=bulbs-autocomplete-group-items><li class=bulbs-autocomplete-item ng-repeat=\"item in group[1]\" ng-click=\"onSelect({selection: item})\" ng-class=\"{active: selectedGroupIndex === $parent.$index && $index === selectedIndex}\" ng-mouseenter=\"mouseEnterItem($parent.$index, $index)\" ng-mouseleave=mouseLeaveItem()>{{ formatter({item:item}) }}</li></ul></li></ul>"
+    "<ul><li class=bulbs-autocomplete-group-container ng-repeat=\"group in groupedItems\"><div class=bulbs-autocomplete-group-key>{{ group[0] }}</div><ul class=bulbs-autocomplete-group-items><li class=bulbs-autocomplete-item ng-repeat=\"item in group[1]\" ng-click=\"onSelect({selection: item})\" ng-class=\"{active: selectedGroupIndex === $parent.$index && $index === selectedIndex}\" ng-mouseenter=\"mouseEnterItem($parent.$index, $index)\" ng-mouseleave=mouseLeaveItem() ng-bind-html=formatter({item:item})></li></ul></li></ul>"
   );
 
 
   $templateCache.put('src/bulbs-autocomplete-suggest/bulbs-autocomplete-suggest/bulbs-autocomplete-suggest.html',
-    "<ul><li ng-repeat=\"item in items\" ng-click=\"onSelect({selection: item})\" ng-class=\"{active: $index === selectedIndex}\" ng-mouseenter=mouseEnterItem($index) ng-mouseleave=mouseLeaveItem()>{{ formatter({item:item}) }}</li></ul>"
+    "<ul><li ng-repeat=\"item in items\" ng-click=\"onSelect({selection: item})\" ng-class=\"{active: $index === selectedIndex}\" ng-mouseenter=mouseEnterItem($index) ng-mouseleave=mouseLeaveItem() ng-bind-html=formatter({item:item})></li></ul>"
   );
 
 }]);
